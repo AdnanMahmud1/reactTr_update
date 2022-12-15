0256363ec3a53adef93078bb75d4570a1268e08a
Adnan Mahmud <11adnan@live.com>
2022-12-15 16:36:25 +0600

    Event Handling & Control Re Rendering

---
+++ src/components/Button.js
@@ -0,0 +1,26 @@
+/* eslint-disable react/prefer-stateless-function */
+import React from 'react';
+
+class Button extends React.Component {
+    shouldComponentUpdate(nextProps) {
+        const { change: currentChange, locale: currentLocale } = this.props;
+        const { change: nextChange, locale: nextLocale } = nextProps;
+        if (currentChange === nextChange && nextLocale === currentLocale) {
+            return false;
+        }
+        return true;
+    }
+
+    render() {
+        console.log('button rendered');
+        const { change, locale } = this.props;
+
+        return (
+            <button type="button" onClick={() => change(locale)}>
+                Click Here
+            </button>
+        );
+    }
+}
+
+export default Button;
--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
+import Button from './Button';
 
 class Clock extends React.Component {
     // constructor(props) {
@@ -10,25 +11,23 @@ class Clock extends React.Component {
     state = { date: new Date(), locale: 'bn-BD' };
 
     componentDidMount() {
-        this.clockTimer = setInterval(() => {
-            this.tick();
-        }, 1000);
+        this.clockTimer = setInterval(() => this.tick(), 1000);
     }
 
     componentWillUnmount() {
-        clearTimeout(this.clockTimer);
+        clearInterval(this.clockTimer);
     }
 
-    // handleClick = () => {
-    //     this.setState({
-    //         locale: this.locale ? 'en-US' : 'bn-BD',
-    //     });
-    // };
-    handleClick(locale) {
+    handleClick = (locale) => {
         this.setState({
             locale,
         });
-    }
+    };
+    // handleClick(locale) {
+    //     this.setState({
+    //         locale,
+    //     });
+    // }
 
     tick() {
         this.setState({
@@ -37,15 +36,16 @@ class Clock extends React.Component {
     }
 
     render() {
+        console.log('clock rendered');
         const { date, locale } = this.state;
         return (
             <div>
                 <h1 className="heading">
                     <p>{date.toLocaleTimeString(locale)} </p>
                 </h1>
-                <button type="button" onClick={() => this.handleClick('en-US')}>
-                    Click Here
-                </button>
+                <Button change={this.handleClick} locale="en-US">
+                    Click
+                </Button>
             </div>
         );
     }
