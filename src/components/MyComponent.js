049c71406d7550ccc9b81739e3831fc40aeb4097
Adnan <Adnan@DESKTOP-UGPNVH0>
2023-01-01 15:56:46 +0600

    UseEffect Hook

---
+++ src/components/MyComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function MyComponent() {
+  return (
+    // <div>
+    //     <p>Time: {date.toLocaleTimeString()}</p>
+    //     <p>
+    //         <button></button>
+    //     </p>
+    // </div>
+  )
+}
